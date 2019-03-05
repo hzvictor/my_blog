e5570e6c33631aa472653cabedc3684d5281adcd
hzvictor <15703903887@163.com>
2019-03-06 00:47:49 +0800

    数据库

--- dao/DButil.js
+++ dao/DButil.js
@@ -2,7 +2,7 @@ var mysql = require("mysql")
 
 function creatConnection(){
     var connection = mysql.createConnection({
-        host: 'localhost',
+        host: '10.13.18.6',
         user: "root",
         password: "root",
         database: "my_blog"
